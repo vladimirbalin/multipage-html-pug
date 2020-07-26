ee3e241246eb083db7428b0b6b3f0d12514dec01
vladimirbalin <vladimirzq@Gmail.com>
2020-07-26 16:53:23 +0600

    project structure, navbar, carousel

---
+++ webpack.config.js
@@ -0,0 +1,141 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyWPPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ImageMinWebpackPlugin = require("imagemin-webpack-plugin").default;
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminPngquant = require("imagemin-pngquant");
+const fs = require("fs");
+
+const isDev = process.env.NODE_ENV === "development";
+const filename = (ext) => (isDev ? `[name].${ext}` : `[name].[hash].${ext}`);
+
+const PAGES = fs
+  .readdirSync(`${__dirname}/src/pug`)
+  .filter((fileName) => fileName.endsWith(".pug"));
+
+const makeCopiesOfFiles = (path) => {
+  return {
+    from: path,
+    to: path,
+  };
+};
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: ["@babel/polyfill", "./js/index.js"],
+  output: {
+    filename: filename("js"),
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    ...PAGES.map(
+      (page) =>
+        new HTMLWebpackPlugin({
+          template: `${__dirname}/src/pug/${page}`,
+          filename: `./${page.replace(/\.pug/, ".html")}`,
+          minify: {
+            collapseWhitespace: !isDev,
+            removeComments: !isDev,
+            removeRedundantAttributes: !isDev,
+            removeScriptTypeAttributes: !isDev,
+            removeStyleLinkTypeAttributes: !isDev,
+            useShortDoctype: !isDev,
+          },
+        })
+    ),
+    new CleanWebpackPlugin(),
+    new CopyWPPlugin([makeCopiesOfFiles("img")]),
+    new ImageMinWebpackPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      disable: isDev,
+      plugins: [
+        imageminMozjpeg({
+          quality: 60,
+          progressive: true,
+        }),
+        imageminPngquant(),
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename("css"),
+    }),
+  ],
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  devServer: {
+    port: 4200,
+    hot: isDev,
+    contentBase: "./src",
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(s[ac]ss)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)/,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]",
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+      },
+    ],
+  },
+};
